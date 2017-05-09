a11bbd9fa532671c5c109a355e9dca43385edacf
P3T3Rr <jeremmysq17@gmail.com>
2017-05-08 23:27:51 -0600

    borrar despiche

--- numerosde1al10.py
+++ numerosde1al10.py
@@ -1,10 +1,7 @@
 def numerosHasta10(num):
-<<<<<<< HEAD
     for x in range(1,num + 1):
         print("El numero actual es: %s"%(x))
-=======
     for x in range(x,y + 1):
         print(x)
->>>>>>> origin/master
 
 numerosDesdeXhastaY(6,9)
