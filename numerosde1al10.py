b52ddf4e1f64edcf80302ec24d5957384b02279a
Alonso Vega <lavb91@gmail.com>
2017-05-08 23:32:28 -0600

    Solucionando conflicto.

--- numerosde1al10.py
+++ numerosde1al10.py
@@ -1,7 +1,5 @@
 def numerosHasta10(num):
-    for x in range(1,num + 1):
-        print("El numero actual es: %s"%(x))
     for x in range(x,y + 1):
-        print(x)
+        print("El numero actual es: %s"%(x))
 
 numerosDesdeXhastaY(6,9)
